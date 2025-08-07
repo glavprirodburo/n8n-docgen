5cd253c576b216baa81e6e162fdf1334ab947cbb
glavprirodburo <zavarzina@glav.pro>
2025-08-07 16:54:30 +0700

    Update main.py2.0

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI
+FROM python:3.11-slim
 
-app = FastAPI()
+WORKDIR /app
 
-@app.get("/")
-def root():
-    return {"status": "ok"}
+COPY ./app/requirements.txt ./requirements.txt
+COPY ./app/main.py ./main.py
 
-@app.get("/ping")
-def ping():
-    return {"ping": "pong"}
+RUN pip install --no-cache-dir -r requirements.txt
+
+EXPOSE 8000
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
